520e9e529625c801e73ee072cf1ee553e0453f78
yadav-saurabh <yadav-saurabh@outlook.com>
2023-10-02 19:56:19 +0530

    templates from shadcn ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,24 +1,86 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+import "@/styles/globals.css";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
+import { LayoutProps } from "@/types";
 
-export const metadata: Metadata = {
-  title: "OpenUtilize",
-  description:
-    "OpenUtilize is an online utility website that provides a suite of browser-based tools to simplify common tasks such as PDF manipulation, file compression, image editing, and more, without the need for users to install additional software.",
-  keywords: ["PDF Tools", "Image Editing"],
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontHeading = localFont({
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
+
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: siteConfig.name,
+      url: siteConfig.url,
+    },
+  ],
+  creator: siteConfig.name,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: `@{siteConfig.name}`,
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
-interface ChildrenProp {
-  children: React.ReactNode;
-}
-
-export default function RootLayout(props: ChildrenProp) {
+export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{props.children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontHeading.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ apps/web/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "OpenUtilize",
+  description:
+    "OpenUtilize is an online utility website that provides a suite of browser-based tools to simplify common tasks such as PDF manipulation, file compression, image editing, and more, without the need for users to install additional software.",
+  url: "https://openutilize.com",
+  ogImage: "https://openutilize.com/og.jpg",
+  links: {
+    github: "https://github.com/koshthak/OpenUtilize",
+  },
+};
