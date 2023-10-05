d3426dcc2c33555af77d1d29a9ef954a8cb2f7f4
yadav-saurabh <yadav-saurabh@outlook.com>
2023-10-05 07:22:55 +0530

    feat(docs): nextra

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,8 @@
+export default {
+  logo: <span>My Nextra Documentation</span>,
+  docsRepositoryBase: "https://github.com/koshthak/OpenUtilize/tree/main/apps/docs",
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // ... other theme options
+};
